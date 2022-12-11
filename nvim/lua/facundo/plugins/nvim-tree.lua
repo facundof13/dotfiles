7f9e79c9d014f71210dc417d0637ca23d30b34de
Facundo Figueroa <facundof13@gmail.com>
2022-12-11 18:26:07 -0500

    updated nvim

--- nvim/lua/facundo/plugins/nvim-tree.lua
+++ nvim/lua/facundo/plugins/nvim-tree.lua
@@ -5,17 +5,19 @@ end
 
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
-vim.g.nvim_tree_respect_buf_cwd = 1
 
 nvimtree.setup({
+  respect_buf_cwd = true,
+  hijack_unnamed_buffer_when_opening = true,
+  ignore_buffer_on_setup = true,
   open_on_setup = true,
   git = {
     ignore = false,
   },
-  update_cwd = true,
+  -- update_cwd = true,
   updated_focused_file = {
     enable = true,
-    update_cwd = true,
+    update_root = true,
   },
   actions = {
     open_file = {
